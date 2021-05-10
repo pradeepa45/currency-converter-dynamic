592d0a78b0e9d087f6a26a1bb04ea55fedfc1ee1
Pradeepa Seelam <39674626+pradeepa45@users.noreply.github.com>
2021-05-11 00:03:49 +0530

    tests done

---
+++ index.php
@@ -0,0 +1,229 @@
+<html>
+<!-- <meta http-equiv="refresh" content="20"> -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- Bootstrap CSS -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<title>
+    Currency convertor
+</title>
+<link href="style.css" rel="stylesheet">
+<script src="sample.js"></script>
+
+<body>
+    </div>
+    <header id="top-bar">
+        <h1 class="header">Currency Converter</h1>
+
+    </header>
+    <div class="container">
+        <div class="p-sm-0 p-4 bg-transparent bg-gradient">
+            <p id="obs-date" style="text-align: left;padding-left: 3px;"> *</p>
+            <div class="right tooltip">
+                How to use?
+                <span class="tooltiptext">
+                    Enter a number in the field opposite to the value you want to convert, and click the convert button.
+                    For a reuse, erase the field you want to use and repeat the above step.
+                </span>
+            </div>
+            <form name="converter">
+                <div class="row g-lg-3">
+                    <div class="col-md-12 col-lg-4">
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="asd" name="asd" placeholder="Australian Dollar"
+                                onChange="asdConverter()" />
+                            <label for="asd">Australian Dollar</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="brl" name="brl" placeholder="Brazilian Real"
+                                onChange="brlConverter()" />
+                            <label for="brl">Brazilian Real</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="bgn" name="bgn" placeholder="Bulgarian Lev"
+                                onChange="bgnConverter()" />
+                            <label for="bgn">Bulgarian Lev</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="cad" name="cad" placeholder="Canadian Dollar"
+                                onChange="cadConverter()" />
+                            <label for="cad">Canadian Dollar</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="yuan" name="yuan" placeholder="Chinese Yuan"
+                                onChange="yuanConverter()" />
+                            <label for="yuan">Chinese Yuan</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="hrk" placeholder="Croatian Kuna" name="hrk"
+                                onChange="hrkConverter()" />
+                            <label for="hrk">Croatian Kuna</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="czk" name="czk" placeholder="Czech Koruna"
+                                onChange="czkConverter()" />
+                            <label for="czk">Czech Koruna</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="dkk" placeholder="Danish Crone" name="dkk"
+                                onChange="dkkConverter()" />
+                            <label for="dkk">Danish Krone</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="euro" placeholder="euro" name="euro"
+                                onChange="euroConverter()" />
+                            <label for="euro">Euro</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="hkd" placeholder="Hong Kong Dollar" name="hkd"
+                                onChange="hkdConverter()" />
+                            <label for="hkd">Hong Kong Dollar</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="huf" placeholder="Hungarian Florint" name="huf"
+                                onChange="hufConverter()" />
+                            <label for="huf">Hungarian Florint</label>
+
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-4">
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="isk" name="isk" placeholder="Icelandic Krona" onChange="iskConverter()" />
+                            <label for="isk">Icelandic Krona</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="inr" name="rupee" placeholder="Indian Rupee" onChange="rupeeConverter()" />
+                            <label for="inr">Indian Rupee</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="idr" placeholder="Indonesian Rupee" name="idr" onChange="idrConverter()" />
+                            <label for="idr">Indonesian Rupee</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="isl" name="isl" placeholder="Israeli New Shekel" onChange="islConverter()" />
+                            <label for="isl">Israeli New Shekel</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="yen" placeholder="Japanese Yen" name="yen" onChange="yenConverter()" />
+                            <label for="yen">Japanese Yen</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="mly" name="ringgit" placeholder="Malaysian Ringgit" onChange="ringgitConverter()" />
+                            <label for="mly">Malaysian Ringgit</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="peso" name="peso" placeholder="Mexican Peso" onChange="pesoConverter()" />
+                            <label for="peso">Mexican Peso</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="nok" name="nok" placeholder="Norwegian Krona" onChange="nokConverter()" />
+                            <label for="nok">Norwegian Krona</label>
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="nzd" placeholder="New Zealand Dollar" name="nzd" onChange="nzdConverter()" />
+                            <label for="nzd">New Zealand Dollar</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="php" name="php" placeholder="Philippine Peso" onChange="phpConverter()" />
+                            <label for="php">Philippine Peso</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="pln" placeholder="Polish Zloty" name="pln" onChange="plnConverter()" />
+                            <label for="pln">Polish Złoty</label>
+
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-4">
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="pound" name="pound" placeholder="Pound Sterling" onChange="poundConverter()" />
+                            <label for="pound">Pound Sterling</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="ron" name="ron" placeholder="Romenian Leu" onChange="ronConverter()" />
+                            <label for="ron">Romenian Leu</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="ruble" name="ruble" placeholder="Russian Ruble" onChange="rubleConverter()" />
+                            <label for="ruble">Russian Ruble</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="sgd" name="sgd" placeholder="Singaporean Dollar" onChange="sgdConverter()" />
+                            <label for="sgd">Singaporean Dollar</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="zar" placeholder="South African Rand" name="zar"  onChange="zarConverter()" />
+                            <label for="zar">South African Rand</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="won" name="won" placeholder="South Korean Won" onChange="skwConverter()" />
+                            <label for="won">South Korean Won</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="sek" name="sek" placeholder="Swedish Krona" onChange="sekConverter()" />
+                            <label for="sek">Swedish Krona</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="franc" placeholder="Swiss Franc" name="franc" onChange="francConverter()" />
+                            <label for="franc">Swiss Franc</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="thb" placeholder="Thai Baht" name="thb" onChange="thbConverter()" />
+                            <label for="thb">Thai Baht</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="tri" placeholder="Turkish Iira" name="tri" onChange="triConverter()" />
+                            <label for="tri">Turkish Iira</label>
+
+                        </div>
+                        <div class="form-floating mb-3">
+                            <input class="form-control" type="text" style="background-color: blanchedalmond" id="dollar" placeholder="US Dollar" name="dollar" onChange="dollarConverter()" />
+                            <label for="dollar">US Dollar</label>
+
+                        </div>
+                    </div>
+                </div>
+                <div class="row row-cols-sm-6 g-3 justify-content-lg-around">
+                    <input type="button" class="btn btn-light"
+                        value="Convert"
+                    >
+                    <button type="reset" class="btn btn-danger">Clear</button>
+                </div>
+        </div>
+        </form>
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8"
+    crossorigin="anonymous"></script>
+</html>
